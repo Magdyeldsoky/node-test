29ab9df6dd005e650f56fd330c944c54e97318b8
Magdyeldsoky <youssifmohamed5590@gmail.com>
2026-03-03 00:39:23 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import router from "./routs/useroute.js";
 import routercar from "./routs/carsrout.js";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://magdy_eldsoky:123456789Magdy@cluster0.gzdtpgy.mongodb.net/?appName=Cluster0",
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use("/cars", routercar);
 app.use("/user", router);
